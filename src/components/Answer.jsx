524045a95dbe88ba90c1461999d9258e023e8335
xet1309 <xavier.tromp@student.hu.nl>
2025-03-12 13:33:42 +0100

    added animation for wrong choice

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,9 +1,21 @@
 import "../styles/Answer.css"
+import {useState} from "react";
 
 const Answer = ({ answer, onClickAnswer }) => {
+    const[isShaking, setIsShaking] = useState(false);
+
+    const handleClick = () => {
+        if (!answer.correct) {
+            setIsShaking(true);
+            setTimeout(() => setIsShaking(false), 750);
+        }
+        onClickAnswer(answer.correct)
+    }
 
     return (
-        <div onClick={() => onClickAnswer(answer.correct)} className="answer-container" >
+        <div onClick={handleClick}
+             className={`answer-container ${isShaking ? "shake" : ""}`}
+        >
             <h3 className="answer-text" >{answer.text}</h3>
         </div>
     )
